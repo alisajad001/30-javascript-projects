eaa7cb62305d38117ae5014aa27f1964c4427083
Ali Sajad <alisjahmdi@gmail.com>
2025-05-15 21:23:18 +0430

    add project 03

---
+++ 03 - CSS Variables/js/script.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll('input');
+
+function handleChange() {
+  const suffix = this.dataset.sizing ? this.dataset.sizing : '' || '';
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('change', handleChange);
+  input.addEventListener('mousemove', handleChange);
+});
